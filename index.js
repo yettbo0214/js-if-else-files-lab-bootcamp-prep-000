d72ed94719e77885471751882eb32b955b7436bb
gl5249328@gmail.com <gl5249328@gmail.com>
2018-07-14 23:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ if (name === "Susan") {
     console.log("The name is correct")
 }
 
-
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
